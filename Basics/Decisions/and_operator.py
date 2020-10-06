6cebbbc93df480c343a195cf16cf464fbb08955a
repl.it user <replituser@example.com>
2020-10-06 17:11:39 +0000

    Added code to demonstrate the use of a logical and operator

---
+++ Basics/Decisions/and_operator.py
@@ -0,0 +1,21 @@
+# Ask for user input
+print("\nHello its me Beep and I am lost in the forest! .. ")
+print("\nWhat did I hear?")
+Hear = input()
+print("\nWhat did I see?")
+See = input()
+
+# Display a message according to input
+if ((Hear == "grrr") and (See == "two red eyes")):
+    print("\nThere is a scary creature. I should get out of here!")
+else:
+    print("\nI am a little scared but I will continue!")
+
+
+
+
+
+
+
+
+
