9464cce7666a8060d358431258514d571c863d77
repl.it user <replituser@example.com>
2020-10-06 16:45:07 +0000

    Added code to demonstrate the use of multiple nested statements

---
+++ Basics/Decisions/nested_decisions/nestception.py
@@ -0,0 +1,35 @@
+# Ask user for location 
+print("\nHello its me Beep and I need help finding my battery")
+print("\nWhere shall I look? In the bedroom, bathroom or lab?")
+room = input()
+
+# Bedroom
+if (room.lower() == "in the bedroom"):
+    print("Where in the bedroom should I look?")
+    bedroom = input()
+
+    if (bedroom.lower() == "under the bed"):
+        print("Found some shoes but no battery")
+    else:
+        print("Found some mess but no battery.")
+
+# Bathroom
+elif (room.lower() == "in the bathroom"):
+    print("Where in the bathroom shall I look?")
+    bathroom = input()
+
+    if (bathroom.lower() == "In the bath tub"):
+        print("Found a rubber ducky but no battery")
+    else:
+       print("Found a wet surface but no battery")
+
+# Lab
+elif (room.lower() == "in the lab"):
+    print("Where in the lab shall I look?")
+    lab_place = input()
+
+    if (lab_place.lower() == "on the table"):
+      print("Hooray! I found my battery!")
+    else:
+       print("Found some tools but no battery")
+    
