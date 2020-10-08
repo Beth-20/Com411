d5958a2dcbbb8bcd946814608a4f2608b1e47a88
repl.it user <replituser@example.com>
2020-10-08 18:41:09 +0000

    Added code to demonstrate the use of summing with a while loop

---
+++ Basics/Repetitions/While_loop/len.py
@@ -0,0 +1,11 @@
+# Ask the user for input
+print("Please enter a phrase")
+phrase = input ()
+
+# Control variable 
+Bop = 0
+
+# While Loop
+while ( Bop < len(phrase)):
+  print(" bop ", end="")
+  Bop = Bop + 1  
---
+++ Basics/Repetitions/While_loop/sum_100.py
@@ -0,0 +1,13 @@
+print("Calculating sum")
+
+# Variables
+number = 1
+total = 0
+
+
+while (number <= 100):
+    total = total + number
+    number = number + 1
+
+# Display result
+print("...Done! The answer is", total)
