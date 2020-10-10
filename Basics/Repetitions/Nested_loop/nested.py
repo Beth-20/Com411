3ec0013f8aeaa58cd5a7bb001a68231721f7602e
repl.it user <replituser@example.com>
2020-10-10 15:44:22 +0000

    Added code to demonstrate one loop nested in another

---
+++ Basics/Repetitions/Nested_loop/nested.py
@@ -0,0 +1,12 @@
+# Ask user for columns and rows
+print("\nHow many rows should I have?")
+rows = int(input())
+print("\n How many columns should I have?")
+columns = int(input())
+print("\nHere I go...")
+
+# Emoji Grid
+for row in range(0, rows, 1):
+    for column in range(0, columns, 1):
+        print(":-)", end="")
+    print()
