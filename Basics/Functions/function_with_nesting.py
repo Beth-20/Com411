e047c8b7e2667fafb60058e0cacc6322adffe3ef
repl.it user <replituser@example.com>
2020-10-13 10:47:04 +0000

    Added code to demonstrate the use of a user defined function with a nested decision

---
+++ Basics/Functions/function_with_nesting.py
@@ -0,0 +1,15 @@
+
+# Define see function
+def identify():
+  # Ask user for what they can see
+  print("\nWhat can you see up ahead?")
+  ahead = input()
+
+  if (ahead.lower() == "a large boulder"):
+    print("Its time to run!")
+  else:
+    print("We will be fine")
+
+    
+# Call to function
+identify()
