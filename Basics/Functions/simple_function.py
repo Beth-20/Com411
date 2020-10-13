e95829e8f69ed5cb4a167a79bf6f163241be45ef
repl.it user <replituser@example.com>
2020-10-13 10:21:06 +0000

    Added code to demonstrate the use of a user defined function

---
+++ Basics/Functions/simple_function.py
@@ -0,0 +1,13 @@
+# Define listen function
+def listen():
+
+    # Ask user for the sound 
+    print("\nWhat sound did I hear?")
+    sound = input() 
+
+    # Display sound
+    print("\nThat was a loud", sound)
+
+
+# Call to function
+listen()
