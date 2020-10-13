45717b5c4476fd151d8bce25c6e9ab4fbc149387
repl.it user <replituser@example.com>
2020-10-13 10:15:35 +0000

    Added code to demonstrate  the use of built in functions len and ord

--- Basics/Functions/ascii_code.py
+++ Basics/Functions/ascii_code.py
@@ -2,8 +2,8 @@ print("\nProgram started!")
 print("\nPlease enter a letter")
 letter = input()
 
-if len(letter == 1):
-    print("The ASCII code for {} is {}".format(ord(letter)))
+if len(letter) == 1:
+    print("The ASCII code for {} is {}".format(letter,ord(letter)))
 else:
     print("Error, more than one character entered")
     
