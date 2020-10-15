92b431b62676bf1dbd7890b4f72b93dc255d1ced
repl.it user <replituser@example.com>
2020-10-15 12:32:23 +0000

    Added code to demonstrate the use of return values

---
+++ Basics/Functions/return_values.py
@@ -0,0 +1,30 @@
+def sum_weights(beep_weight, bop_weight):
+    total_weight = beep_weight + bop_weight
+    return total_weight
+
+def calc_avg_weight(beep_weight, bop_weight):
+    avg_weight = (beep_weight + bop_weight) / 2
+    return avg_weight
+
+def run():
+    # Ask user for weight
+    print("What is the weight of Bop?")
+    bop_weight = float(input())
+
+    print("What is the weight of Beep?")
+    beep_weight = float(input())
+
+    print("What would you like to calculate (sum or average)?")
+    calculation = input()
+
+    # Carry out calculation
+    if (calculation == "sum"):
+        answer = sum_weights(beep_weight, bop_weight)
+        print("The sum of Beep's and Bop's weight is {:.2f}".format(answer))
+    elif (calculation == "average"):
+        answer = calc_avg_weight(beep_weight, bop_weight)
+        print("The average of Beep's and Bop's weight is {:.2f}".format(answer))
+    else:
+        print("I am not sure what you would like to do.")
+# Call the function
+run()
