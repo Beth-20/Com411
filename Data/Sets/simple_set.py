fafce32fdaa6ae6963967168bc182ef68e67215a
repl.it user <replituser@example.com>
2020-10-27 10:19:39 +0000

    Added code to demonstrate a simple set

---
+++ Data/Sets/simple_set.py
@@ -0,0 +1,12 @@
+# define function 1
+def observed (): 
+  observations = {"Flying Car", "Sky Scraper", "Laser", "Dome"}
+
+  return observations
+
+# define function 2
+def run():
+
+  print(observed())
+
+run()
