1fc4cd7b50c7c912c278c5f52ed2acba574326ed
repl.it user <replituser@example.com>
2020-12-12 10:58:39 +0000

    Added code to create a robot class

---
+++ oop/planet.py
@@ -0,0 +1,25 @@
+class Planet:
+
+  def __init__(self):
+    self.humans = []
+    self.robots = []
+
+def add_human(self, human):
+  self.humans.append(human)
+
+def remove_human(self, human):
+  self.humans.remove(human)
+
+def add_robot(self, robot):
+  self.robots.append(robot)
+
+def remove_robot(self, robot):
+  self.robots.remove(robot)
+
+planet_mars = Planet()
+beth = human (Beth)
+robot_beth = robot(BethR)
+planet_mars.add_human(beth)
+planet_mars.add_robot(robot_beth)
+
+print(Planet)
